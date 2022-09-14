efdb4b6022b17ee679a75339834b2ed5ee78379a
NUEVOESPACIO <sebacap3@hotmail.com>
2022-09-14 11:18:14 -0300

    No version de Java en POM y Agregado de CrossOrigin en ENDpoint

--- src/main/java/com/bkend3/SpringBoot/Controller/Controller.java
+++ src/main/java/com/bkend3/SpringBoot/Controller/Controller.java
@@ -1,14 +1,16 @@
 
 package com.bkend3.SpringBoot.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     
+    @CrossOrigin(origins = {"http://localhost:4200", "https://angularseba.web.app"})
     @GetMapping("/hola")
     public String decirHola(){
-        return "Bienvendio a BK";
+        return "Bienvendio a Back End Sebas";
     }
 }
