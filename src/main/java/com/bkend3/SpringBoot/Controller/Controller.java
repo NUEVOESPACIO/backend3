12bf4938e4c43dddb437658d7e0f313d7ae4c5e1
NUEVOESPACIO <sebacap3@hotmail.com>
2022-09-18 13:46:05 -0300

    RequestBody con metodo POST para new/pais

--- src/main/java/com/bkend3/SpringBoot/Controller/Controller.java
+++ src/main/java/com/bkend3/SpringBoot/Controller/Controller.java
@@ -4,6 +4,7 @@ package com.bkend3.SpringBoot.Controller;
 import com.bkend3.SpringBoot.model.Pais;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class Controller {
     }
     
     @CrossOrigin(origins = {"http://localhost:4200", "https://angularseba.web.app"})
-    @GetMapping("/new/pais")
+    @PostMapping("/new/pais")
     public void agregarPais(@RequestBody Pais pai){
         
     }
